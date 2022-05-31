33f9c1fa0efd778de85c370a3c8985de070f3351
Jimmy Osma <josma109@gmail.com>
2022-05-31 14:22:10 -0500

    update login with social networks

--- src/JournalApp.js
+++ src/JournalApp.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
 
 import { AppRouter } from './routers/AppRouter'
 import { store } from './store/store'
